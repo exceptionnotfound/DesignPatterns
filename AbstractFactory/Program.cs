407d2dec8e2cd0bbcf268598bf92b0c66debc1a0
exceptionnotfound <mpjones428@gmail.com>
2016-05-16 08:07:57 -0700

    Finished AbstractFactory pattern

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -10,6 +10,31 @@ namespace AbstractFactory
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Who are you? (R)ebellion or (E)mpire?");
+            char input = Console.ReadKey().KeyChar;
+            StarshipFactory factory;
+            switch(input)
+            {
+                case 'E':
+                    factory = new EmpireFactory();
+                    break;
+
+                case 'R':
+                    factory = new RebellionFactory();
+                    break;
+
+                default:
+                    throw new NotImplementedException();
+
+            }
+
+            var fighter = factory.CreateStarfighter();
+            var capitalShip = factory.CreateCapitalShip();
+
+            Console.WriteLine("\nFighter: " + fighter.GetType().Name);
+            Console.WriteLine("Capital Ship: " + capitalShip.GetType().Name);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ AbstractFactory/StarshipFactory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    abstract class Starfighter { }
+
+    abstract class CapitalShip { }
+
+    abstract class SuperWeapon { }
+
+    abstract class StarshipFactory
+    {
+        public abstract Starfighter CreateStarfighter();
+        public abstract CapitalShip CreateCapitalShip();
+    }
+
+    class TIEFighter : Starfighter { }
+
+    class StarDestroyer : CapitalShip { }
+
+
+    class EmpireFactory : StarshipFactory
+    {
+        public override Starfighter CreateStarfighter()
+        {
+            return new TIEFighter();
+        }
+
+        public override CapitalShip CreateCapitalShip()
+        {
+            return new StarDestroyer();
+        }
+    }
+
+    class XWing : Starfighter { }
+
+    class MonCalamariCruiser : CapitalShip { }
+
+    class RebellionFactory : StarshipFactory
+    {
+        public override Starfighter CreateStarfighter()
+        {
+            return new XWing();
+        }
+
+        public override CapitalShip CreateCapitalShip()
+        {
+            return new MonCalamariCruiser();
+        }
+    }
+}
