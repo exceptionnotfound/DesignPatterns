c872acf49137245325397db5613b03d76301dcec
exceptionnotfound <mpjones428@gmail.com>
2016-05-16 07:38:27 -0700

    Generated a FactoryMethod pattern example.

---
+++ FactoryMethodPattern/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Starfighter> fighters = new List<Starfighter>();
+            fighters.Add(new XWing());
+            fighters.Add(new YWing());
+            fighters.Add(new AWing());
+
+            foreach(var starfighter in fighters)
+            {
+                Console.WriteLine("Craft: " + starfighter.GetType().Name + " ");
+                foreach(var weapon in starfighter.Weapons)
+                {
+                    Console.WriteLine("Weapon: " + weapon.GetType().Name);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FactoryMethodPattern/Starfighter.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPattern
+{
+    /// <summary>
+    /// Product
+    /// </summary>
+    abstract class Weapon {
+        public string Description { get; set; }
+    }
+
+    /// <summary>
+    /// ConcreteProduct
+    /// </summary>
+    class LaserCannon : Weapon
+    {
+
+    }
+
+    /// <summary>
+    /// ConcreteProduct
+    /// </summary>
+    class ProtonTorpedoLauncher : Weapon
+    {
+
+    }
+
+    /// <summary>
+    /// ConcreteProduct
+    /// </summary>
+    class IonCannon : Weapon
+    {
+
+    }
+
+    /// <summary>
+    /// ConcreteProduct
+    /// </summary>
+    class ConcussionMissile : Weapon
+    {
+
+    }
+
+
+    /// <summary>
+    /// Creator
+    /// </summary>
+    abstract class Starfighter
+    {
+        private List<Weapon> _weapons = new List<Weapon>();
+
+        public Starfighter()
+        {
+            CreateWeapons();
+        }
+
+        //Factory method
+        public abstract void CreateWeapons();
+
+        public List<Weapon> Weapons
+        {
+            get { return _weapons; }
+        }
+    }
+
+    /// <summary>
+    /// ConcreteCreator
+    /// </summary>
+    class XWing : Starfighter
+    {
+        public override void CreateWeapons()
+        {
+            Weapons.Add(new LaserCannon());
+            Weapons.Add(new LaserCannon());
+            Weapons.Add(new LaserCannon());
+            Weapons.Add(new LaserCannon());
+            Weapons.Add(new ProtonTorpedoLauncher());
+            Weapons.Add(new ProtonTorpedoLauncher());
+        }
+    }
+
+    /// <summary>
+    /// ConcreteCreator
+    /// </summary>
+    class YWing : Starfighter
+    {
+        public override void CreateWeapons()
+        {
+            Weapons.Add(new LaserCannon());
+            Weapons.Add(new LaserCannon());
+            Weapons.Add(new ProtonTorpedoLauncher());
+            Weapons.Add(new ProtonTorpedoLauncher());
+            Weapons.Add(new IonCannon());
+            Weapons.Add(new IonCannon());
+        }
+    }
+
+    /// <summary>
+    /// ConcreteCreator
+    /// </summary>
+    class AWing : Starfighter
+    {
+        public override void CreateWeapons()
+        {
+            Weapons.Add(new LaserCannon());
+            Weapons.Add(new LaserCannon());
+            Weapons.Add(new ConcussionMissile());
+            Weapons.Add(new ConcussionMissile());
+        }
+    }
+}
