5e4465bb185be442e448cb3e1afedc98cfb77170
exceptionnotfound <mpjones428@gmail.com>
2016-05-16 08:16:01 -0700

    Added Singleton example.

---
+++ Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var theForce = TheForce.Instance;
+            theForce.DisplayConnection();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/TheForce.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public sealed class TheForce
+    {
+        private static TheForce forceConnection;
+        private static object syncRoot = new Object();
+        private TheForce()
+        {
+
+        }
+
+        /// <summary>
+        /// We implement this method to ensure thread safety for our singleton.
+        /// </summary>
+        public static TheForce Instance
+        {
+            get
+            {
+                lock(syncRoot)
+                {
+                    if(forceConnection == null)
+                    {
+                        forceConnection = new TheForce();
+                    }
+                }
+
+                return forceConnection;
+            }
+        }
+
+        public void DisplayConnection()
+        {
+            Console.WriteLine("Use the Force, Luke!");
+        }
+    }
+}
