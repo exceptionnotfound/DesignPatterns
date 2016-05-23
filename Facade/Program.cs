f8584f1964fa8a4b2f29f4b13771f3345942051f
exceptionnotfound <mpjones428@gmail.com>
2016-05-23 10:07:08 -0700

    Added Facade pattern.

---
+++ Facade/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+
+            Console.WriteLine("Hello!  I'll be your server today. What is your name?");
+            var name = Console.ReadLine();
+
+            Patron patron = new Patron(name);
+
+            Console.WriteLine("Hello " + patron.Name + ". What appetizer would you like? (1-15):");
+            var appID = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("That's a good one.  What entree would you like? (1-20):");
+            var entreeID = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A great choice!  Finally, what drink would you like? (1-60):");
+            var drinkID = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("I'll get that order in right away.");
+
+            server.PlaceOrder(patron, appID, entreeID, drinkID);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Facade/Restaurant.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    class SellableItem { public int DishID; }
+    class HotEntree : SellableItem { }
+    class ColdApp : SellableItem { }
+    class Drink : SellableItem { }
+
+    class Order
+    {
+        public ColdApp Appetizer { get; set; }
+        public HotEntree Entree { get; set; }
+        public Drink Drink { get; set; }
+    }
+
+    /// <summary>
+    /// Patron of the restaurant
+    /// </summary>
+    class Patron
+    {
+        private string _name;
+
+        // Constructor
+        public Patron(string name)
+        {
+            this._name = name;
+        }
+
+        // Gets the name
+        public string Name
+        {
+            get { return _name; }
+        }
+    }
+
+
+    /// <summary>
+    /// The 'Subsystem ClassA' class
+    /// </summary>
+    class ColdPrep
+    {
+        public ColdApp GetColdApp(int dishID)
+        {
+            //Go prep the appetizer
+            return new ColdApp()
+            {
+                DishID = dishID
+            };
+        }
+    }
+
+    /// <summary>
+    /// The 'Subsystem ClassB' class
+    /// </summary>
+    class HotPrep
+    {
+        public HotEntree GetHotEntree(int dishID)
+        {
+            //Go prep the entree
+            return new HotEntree()
+            {
+                DishID = dishID
+            };
+        }
+    }
+
+    /// <summary>
+    /// The 'Subsystem ClassC' class
+    /// </summary>
+    class Bar
+    {
+        public Drink GetDrink(int dishID)
+        {
+            //Go mix the drink
+            return new Drink()
+            {
+                DishID = dishID
+            };
+        }
+    }
+
+    /// <summary>
+    /// The 'Facade' class
+    /// </summary>
+    class Server
+    {
+        private ColdPrep _coldPrep = new ColdPrep();
+        private Bar _bar = new Bar();
+        private HotPrep _hotPrep = new HotPrep();
+
+        public Order PlaceOrder(Patron patron, int coldAppID, int hotEntreeID, int drinkID)
+        {
+            Console.WriteLine("{0} places order for cold app #" + coldAppID.ToString()
+                                + ", hot entree #" + hotEntreeID.ToString()
+                                + ", and drink #" + drinkID.ToString() + ".");
+
+            Order order = new Order();
+
+            order.Appetizer = _coldPrep.GetColdApp(coldAppID);
+            order.Entree = _hotPrep.GetHotEntree(hotEntreeID);
+            order.Drink = _bar.GetDrink(drinkID);
+
+            return order;
+        }
+    }
+}
