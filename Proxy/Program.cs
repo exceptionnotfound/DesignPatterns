f2240edad548af7137795a66a6f995de78d04482
exceptionnotfound <mpjones428@gmail.com>
2016-05-23 10:49:13 -0700

    Added Proxy pattern.

---
+++ Proxy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerProxy proxy = new ServerProxy();
+
+            //Manage orders from a table
+            Console.WriteLine("What would you like to order? ");
+            string order = Console.ReadLine();
+            proxy.TakeOrder(order);
+            Console.WriteLine("Sure thing!  Here's your " + proxy.DeliverOrder() + ".");
+            Console.WriteLine("How would you like to pay?");
+            string payment = Console.ReadLine();
+            proxy.ProcessPayment(payment);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Proxy/Server.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    /// <summary>
+    /// The Subject interface which both the real subject and proxy will need to implement
+    /// </summary>
+    public interface IServer
+    {
+        void TakeOrder(string order);
+        string DeliverOrder();
+        void ProcessPayment(string payment);
+    }
+
+    /// <summary>
+    /// The real subject class which the Proxy can stand in for
+    /// </summary>
+    class Server : IServer
+    {
+        private string Order;
+        public void TakeOrder(string order)
+        {
+            Console.WriteLine("Server takes order for " + order + ".");
+            Order = order;
+        }
+
+        public string DeliverOrder()
+        {
+            return Order;
+        }
+
+        public void ProcessPayment(string payment)
+        {
+            Console.WriteLine("Payment for order (" + payment + ") processed.");
+        }
+    }
+
+    /// <summary>
+    /// The Proxy class, which can substitute for the Real Subject.
+    /// </summary>
+    class ServerProxy : IServer
+    {
+        private Server _server = new Server();
+
+        public void TakeOrder(string order)
+        {
+            _server.TakeOrder(order);
+        }
+
+        public string DeliverOrder()
+        {
+            return _server.DeliverOrder();
+        }
+
+        public void ProcessPayment(string payment)
+        {
+            _server.ProcessPayment(payment);
+        }
+    }
+}
