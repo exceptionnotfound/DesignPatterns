b491c9621f255ebecb8156781e31cb28845bacc5
exceptionnotfound <mpjones428@gmail.com>
2016-05-25 08:22:29 -0700

    Added Strategy pattern.

---
+++ Strategy/CookMethod.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    /// <summary>
+    /// The Strategy abstract class
+    /// </summary>
+    abstract class CookStrategy
+    {
+        public abstract void Cook(string food);
+    }
+
+    /// <summary>
+    /// A ConcreteStrategy class
+    /// </summary>
+    class Grilling : CookStrategy
+    {
+        public override void Cook(string food)
+        {
+            Console.WriteLine("\nCooking " + food + " by grilling it.");
+        }
+    }
+
+    /// <summary>
+    /// A ConcreteStrategy class
+    /// </summary>
+    class OvenBaking : CookStrategy
+    {
+        public override void Cook(string food)
+        {
+            Console.WriteLine("\nCooking " + food + " by oven baking it.");
+        }
+    }
+
+    /// <summary>
+    /// A ConcreteStrategy class
+    /// </summary>
+    class DeepFrying : CookStrategy
+    {
+        public override void Cook(string food)
+        {
+            Console.WriteLine("\nCooking " + food + " by deep frying it");
+        }
+    }
+
+    /// <summary>
+    /// The Context class
+    /// </summary>
+    class CookingMethod
+    {
+        private string Food;
+        private CookStrategy _sortstrategy;
+
+        public void SetSortStrategy(CookStrategy sortstrategy)
+        {
+            this._sortstrategy = sortstrategy;
+        }
+
+        public void SetFood(string name)
+        {
+            Food = name;
+        }
+
+        public void Sort()
+        {
+            _sortstrategy.Cook(Food);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CookingMethod cookMethod = new CookingMethod();
+
+            Console.WriteLine("What food would you like to cook?");
+            var food = Console.ReadLine();
+            cookMethod.SetFood(food);
+
+            Console.WriteLine("What cooking method would you like to use (1-3)?");
+            int input = int.Parse(Console.ReadKey().KeyChar.ToString());
+            
+
+            switch(input)
+            {
+                case 1:
+                    cookMethod.SetSortStrategy(new Grilling());
+                    cookMethod.Sort();
+                    break;
+
+                case 2:
+                    cookMethod.SetSortStrategy(new OvenBaking());
+                    cookMethod.Sort();
+                    break;
+
+                case 3:
+                    cookMethod.SetSortStrategy(new DeepFrying());
+                    cookMethod.Sort();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Selection!");
+                    break;
+            }
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
