cf52d90bcc92fe92a4360ad6f0f5736892978a67
exceptionnotfound <mpjones428@gmail.com>
2016-05-25 10:08:33 -0700

    Added State pattern.

---
+++ State/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Let's cook a steak!
+            Steak account = new Steak("Me");
+
+            // Apply temperature changes
+            account.AddTemp(120);
+            account.AddTemp(15);
+            account.AddTemp(15);
+            account.RemoveTemp(10);
+            account.RemoveTemp(15);
+            account.AddTemp(20);
+            account.AddTemp(20);
+            account.AddTemp(20);
+
+            Console.ReadKey();
+        }
+    }
+}
