7aa5a0c0e199d1f525baf0d4ecb2dc70225a035a
exceptionnotfound <mpjones428@gmail.com>
2016-05-25 09:38:27 -0700

    Added Command pattern.

---
+++ Command/Ordering.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+
+    /// <summary>
+    /// The Receiver abstract class
+    /// </summary>
+    public abstract class Order
+    {
+        List<MenuItem> items { get; set; }
+    }
+
+    /// <summary>
+    /// The Invoker
+    /// </summary>
+    public class FastFoodOrder : Order
+    {
+        List<MenuItem> currentItems { get; set; }
+        public FastFoodOrder()
+        {
+            currentItems = new List<MenuItem>();
+        }
+
+        public void SubmitCommand(int commandOption, MenuItem item)
+        {
+            OrderCommand command = new CommandFactory().GetCommand(commandOption);
+            command.Execute(currentItems, item);
+        }
+
+        public void ShowCurrentItems()
+        {
+            foreach(var item in currentItems)
+            {
+                item.Display();
+            }
+            Console.WriteLine("-----------------------");
+        }
+
+    }
+
+    /// <summary>
+    /// Represents an item being ordered from this restaurant.
+    /// </summary>
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+
+        public MenuItem(string name, int amount, double price)
+        {
+            Name = name;
+            Amount = amount;
+            Price = price;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("\nName: " + Name);
+            Console.WriteLine("Amount: " + Amount.ToString());
+            Console.WriteLine("Price: $" + Price.ToString());
+        }
+    }
+
+    /// <summary>
+    /// The Command interface
+    /// </summary>
+    public abstract class OrderCommand
+    {
+        public abstract List<MenuItem> Execute(List<MenuItem> currentItems, MenuItem newItem);
+    }
+
+    /// <summary>
+    /// A concrete command
+    /// </summary>
+    public class AddCommand : OrderCommand
+    {
+        public override List<MenuItem> Execute(List<MenuItem> currentItems, MenuItem newItem)
+        {
+            currentItems.Add(newItem);
+            return currentItems;
+        }
+    }
+
+    /// <summary>
+    /// A concrete command
+    /// </summary>
+    public class RemoveCommand : OrderCommand
+    {
+        public override List<MenuItem> Execute(List<MenuItem> currentItems, MenuItem newItem)
+        {
+            currentItems.Remove(currentItems.Where(x=>x.Name == newItem.Name).First());
+            return currentItems;
+        }
+    }
+
+    /// <summary>
+    /// A concrete command
+    /// </summary>
+    public class ModifyCommand : OrderCommand
+    {
+        public override List<MenuItem> Execute(List<MenuItem> currentItems, MenuItem newItem)
+        {
+            var item = currentItems.Where(x => x.Name == newItem.Name).First();
+            item.Price = newItem.Price;
+            item.Amount = newItem.Amount;
+            return currentItems;
+        }
+    }
+
+    public class CommandFactory
+    {
+        //Factory method
+        public OrderCommand GetCommand(int commandOption)
+        {
+            switch (commandOption)
+            {
+                case 1:
+                    return new AddCommand();
+                case 2:
+                    return new ModifyCommand();
+                case 3:
+                    return new RemoveCommand();
+                default:
+                    return new AddCommand();
+            }
+        }
+    }
+}
---
+++ Command/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    class Program
+    {
+        /// <summary>
+        /// The Command pattern encapsulates commands as objects, allowing them to be executed by a Receiver class and kicked off
+        /// by an Invoker object.  This enables more complex architectures such as CQRS (Command/Query Responsibility Segregation).
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            FastFoodOrder order = new FastFoodOrder();
+
+            //Add some items
+            order.SubmitCommand(1 /*Add*/, new MenuItem("French Fries", 2, 1.99));
+            order.SubmitCommand(1 /*Add*/, new MenuItem("Hamburger", 2, 2.59));
+            order.SubmitCommand(1 /*Add*/, new MenuItem("Drink", 2, 1.19));
+
+            order.ShowCurrentItems();
+
+            //Remove the french fries
+            order.SubmitCommand(3 /*Remove*/, new MenuItem("French Fries", 2, 1.19));
+
+            order.ShowCurrentItems();
+
+            //Now we want 4 hamburgers rather than 2
+            order.SubmitCommand(2 /*Modify*/, new MenuItem("Hamburger", 4, 2.79));
+
+            order.ShowCurrentItems();
+            Console.ReadKey();
+        }
+    }
+}
