67c3a007cf7417b444a7e7df75b873057ef657b7
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2020-05-05 11:02:35 -0700

    Replaced Composite pattern with .NET Core project.

---
+++ CompositeCore/Drinks.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    /// <summary>
+    /// Component abstract class
+    /// </summary>
+    public abstract class SoftDrink
+    {
+        public int Calories { get; set; }
+
+        public List<SoftDrink> Flavors { get; set; }
+
+        public SoftDrink(int calories)
+        {
+            Calories = calories;
+            Flavors = new List<SoftDrink>();
+        }
+
+        /// <summary>
+        /// "Flatten" method, returns all available flavors
+        /// </summary>
+        public void DisplayCalories()
+        {
+            Console.WriteLine(this.GetType().Name + ": " + this.Calories.ToString() + " calories.");
+            foreach (var drink in this.Flavors)
+            {
+                drink.DisplayCalories();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Leaf class
+    /// </summary>
+    public class VanillaCola : SoftDrink
+    {
+        public VanillaCola(int calories) : base(calories) { }
+    }
+
+    /// <summary>
+    /// Leaf class
+    /// </summary>
+    public class CherryCola : SoftDrink
+    {
+        public CherryCola(int calories) : base(calories) { }
+    }
+
+    /// <summary>
+    /// Leaf class
+    /// </summary>
+    public class StrawberryRootBeer : SoftDrink
+    {
+        public StrawberryRootBeer(int calories) : base(calories) { }
+    }
+
+    /// <summary>
+    /// Leaf class
+    /// </summary>
+    public class VanillaRootBeer : SoftDrink
+    {
+        public VanillaRootBeer(int calories) : base(calories) { }
+    }
+
+    /// <summary>
+    /// Composite class
+    /// </summary>
+    public class Cola : SoftDrink
+    {
+        public Cola(int calories) : base(calories) { }
+    }
+
+    /// <summary>
+    /// Leaf class
+    /// </summary>
+    public class LemonLime : SoftDrink
+    {
+        public LemonLime(int calories) : base(calories) { }
+    }
+
+    /// <summary>
+    /// Composite class
+    /// </summary>
+    public class RootBeer : SoftDrink
+    {
+        public RootBeer(int calories) : base(calories) { }
+    }
+
+    /// <summary>
+    /// Composite class, root node
+    /// </summary>
+    public class SodaWater : SoftDrink
+    {
+        public SodaWater(int calories) : base(calories) { }
+    }
+}
---
+++ CompositeCore/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var colas = new Cola(210);
+            colas.Flavors.Add(new VanillaCola(215));
+            colas.Flavors.Add(new CherryCola(210));
+
+            var lemonLime = new LemonLime(185);
+
+            var rootBeers = new RootBeer(195);
+            rootBeers.Flavors.Add(new VanillaRootBeer(200));
+            rootBeers.Flavors.Add(new StrawberryRootBeer(200));
+
+            SodaWater sodaWater = new SodaWater(180);
+            sodaWater.Flavors.Add(colas);
+            sodaWater.Flavors.Add(lemonLime);
+            sodaWater.Flavors.Add(rootBeers);
+
+            sodaWater.DisplayCalories();
+
+            Console.ReadKey();
+        }
+    }
+}
