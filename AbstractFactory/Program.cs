5dbd2006c432e306b91fab5b3289a982f6449597
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2020-05-05 11:13:25 -0700

    Revert "Converting projects to .NET Core 3.0"
    This reverts commit 49165e0fbf6e562b331c30e5e80fde16f0cefb63.

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -6,41 +6,41 @@ using System.Threading.Tasks;
 
 namespace AbstractFactory
 {
-    class Program
+class Program
+{
+    /// <summary>
+    /// The Abstract Factory pattern provides an interface for creating related families of objects
+    /// without needing to specify the concrete implementations.  This pattern is critical for ideas
+    /// like Dependency Injection.
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
     {
-        /// <summary>
-        /// The Abstract Factory pattern provides an interface for creating related families of objects
-        /// without needing to specify the concrete implementations.  This pattern is critical for ideas
-        /// like Dependency Injection.
-        /// </summary>
-        /// <param name="args"></param>
-        static void Main(string[] args)
+        Console.WriteLine("Who are you? (A)dult or (C)hild?");
+        char input = Console.ReadKey().KeyChar;
+        RecipeFactory factory;
+        switch(input)
         {
-            Console.WriteLine("Who are you? (A)dult or (C)hild?");
-            char input = Console.ReadKey().KeyChar;
-            RecipeFactory factory;
-            switch(input)
-            {
-                case 'A':
-                    factory = new AdultCuisineFactory();
-                    break;
+            case 'A':
+                factory = new AdultCuisineFactory();
+                break;
 
-                case 'C':
-                    factory = new KidCuisineFactory();
-                    break;
+            case 'C':
+                factory = new KidCuisineFactory();
+                break;
 
-                default:
-                    throw new NotImplementedException();
+            default:
+                throw new NotImplementedException();
 
-            }
-
-            var sandwich = factory.CreateSandwich();
-            var dessert = factory.CreateDessert();
-
-            Console.WriteLine("\nSandwich: " + sandwich.GetType().Name);
-            Console.WriteLine("Dessert: " + dessert.GetType().Name);
-
-            Console.ReadKey();
         }
+
+        var sandwich = factory.CreateSandwich();
+        var dessert = factory.CreateDessert();
+
+        Console.WriteLine("\nSandwich: " + sandwich.GetType().Name);
+        Console.WriteLine("Dessert: " + dessert.GetType().Name);
+
+        Console.ReadKey();
     }
+}
 }
